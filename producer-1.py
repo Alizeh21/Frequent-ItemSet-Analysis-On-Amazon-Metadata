bf541f6a8f9bea39c4fe35b16e6b39d4c023c7cd
Moaz Murtaza <123573245+moaz-murtaza@users.noreply.github.com>
2024-04-24 23:05:05 +0500

    Update producer-1.py

--- producer-1.py
+++ producer-1.py
@@ -2,50 +2,55 @@ import json
 import time
 from kafka import KafkaProducer
 
-#Define the path to the preprocessed file 
 file_path = 'preprocessed_data.json'
-
-#Define kafka producer configuration
 bootstrap_servers = ['localhost:9092']
-topic = 'also-buy-topic'
+also_buy_topic = 'also-buy-topic'
 feature_topic = 'product-feature-topic'
-#Initializing the producer 
+
 producer = KafkaProducer(
     bootstrap_servers=bootstrap_servers,
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
-# Defining the batch size and the slide size for sending the data to kafka
 batch_size = 1000
 slide = 500
 
-#Reading the preprocessed data 
 def create_batches(file_path, batch_size):
     with open(file_path, 'r') as file:
-        batch = []       
+        batch = []
         for line in file:
             batch.append(line)
             if len(batch) == batch_size:
                 yield batch
                 batch = []
-        # Don't forget to yield the last batch if it's smaller than the batch size
         if batch:
             yield batch
 
 # Process and send batches of data
-for batch in create_batches(file_path, batch_size):    
-    #extracting the also-buy field from each line in the batch
+for batch in create_batches(file_path, batch_size):
     also_buy_batch = [json.loads(line).get('also_buy', []) for line in batch]
-    feature_batch = [json.loads(line).get('feature', []) for line in batch]
-    #sending the batch of data
-    producer.send(topic, also_buy_batch)
-    print(f"send topic with batch : {also_buy_batch}")
+    
+    feature_batch = [
+    {
+        'asin': json.loads(line).get('asin'),
+        'price': json.loads(line).get('price')
+    }
+    for line in batch
+    if 'asin' in line and
+       json.loads(line).get('price') is not None and
+       isinstance(json.loads(line).get('price'), (int, float)) and
+       json.loads(line).get('price') <= 10000000000
+    ]
+
+    # Send also_buy data to also-buy-topic
+    producer.send(also_buy_topic, also_buy_batch)
+    print(f"Sent batch to {also_buy_topic}: {len(also_buy_batch)} records sent.")
+
+    # Send feature data to product-feature-topic
     producer.send(feature_topic, feature_batch)
     print(f"Sent batch to {feature_topic}: {len(feature_batch)} records sent.")
-    
+
     producer.flush()
-    
-    #Adding delay before sending the next batch
     time.sleep(10)
 
 producer.close()
